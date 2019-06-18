d19a103c6975272604630679db456a6382a3829e
rigatol <rigatol@previnet.previdom.it>
2019-06-18 10:40:32 +0200

    Add intersection linked list problem

--- app/src/test/java/previmedical/it/leetcode/crack/LinkedListTests.kt
+++ app/src/test/java/previmedical/it/leetcode/crack/LinkedListTests.kt
@@ -3,6 +3,7 @@ package previmedical.it.leetcode.crack
 import junit.framework.Assert
 import org.junit.Test
 import previmedical.it.leetcode.models.ListNode
+import previmedical.it.leetcode.problems.crack.linkedList.Intersection
 import previmedical.it.leetcode.problems.crack.linkedList.PalindromeLinkedList
 
 class LinkedListTests {
@@ -33,6 +34,33 @@ class LinkedListTests {
 
         Assert.assertEquals(true, isPalindrome.isPalindromeRecurse(palindromeHead))
         Assert.assertEquals(false, isPalindrome.isPalindromeRecurse(notPalindromeHead))
+    }
+
+    @Test
+    fun intersection() {
+
+        val head = ListNode(1)
+        head.next = ListNode(2)
+        head.next.next = ListNode(3)
+        head.next.next.next = ListNode(4)
+        head.next.next.next.next = ListNode(3)
+        head.next.next.next.next.next = ListNode(2)
+        head.next.next.next.next.next.next = ListNode(1)
+
+        val head2intersect = ListNode(5)
+        head2intersect.next = ListNode(6)
+        head2intersect.next.next = ListNode(7)
+        head2intersect.next.next.next = head.next.next.next
+
+        val head2NonIntersect = ListNode(5)
+        head2NonIntersect.next = ListNode(6)
+        head2NonIntersect.next.next = ListNode(7)
+
+        val intersection = Intersection()
+
+        Assert.assertEquals(head.next.next.next, intersection.intersection(head, head2intersect))
+        Assert.assertEquals(null, intersection.intersection(head, head2NonIntersect))
+
     }
 
 }
