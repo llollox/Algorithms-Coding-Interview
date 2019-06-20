7a94b2306e450fe62c44dafc6d5a115ebc2b5776
rigatol <rigatol@previnet.previdom.it>
2019-06-20 16:41:57 +0200

    Add insertion bit manipulation problem

---
+++ app/src/main/java/previmedical/it/leetcode/problems/crack/treegraph/ValidateBST.java
@@ -0,0 +1,14 @@
+package previmedical.it.leetcode.problems.crack.treegraph;
+
+import previmedical.it.leetcode.models.TreeNode;
+
+public class ValidateBST {
+
+    /*
+        Implement a function to check if a binary tree is a binary search tree.
+     */
+
+//    public boolean validateBST(TreeNode root) {
+//
+//    }
+}
