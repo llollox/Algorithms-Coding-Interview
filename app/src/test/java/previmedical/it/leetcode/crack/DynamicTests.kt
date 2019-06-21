aa082c838dbbbe7aa1830c22808ad1b4a5837065
llollox <lore91tanz@gmail.com>
2019-06-21 06:14:01 +0200

    Add robot in grid implementation

--- app/src/test/java/previmedical/it/leetcode/crack/DynamicTests.kt
+++ app/src/test/java/previmedical/it/leetcode/crack/DynamicTests.kt
@@ -2,6 +2,7 @@ package previmedical.it.leetcode.crack
 
 import org.junit.Assert
 import org.junit.Test
+import previmedical.it.leetcode.problems.crack.dynamic.RobotIntoGrid
 import previmedical.it.leetcode.problems.crack.dynamic.TripleSteps
 
 class DynamicTests {
@@ -28,5 +29,35 @@ class DynamicTests {
         Assert.assertEquals(2555757, tripleSteps.tripleStepDynamicProgramming(25))
         Assert.assertEquals(1132436852, tripleSteps.tripleStepDynamicProgramming(35))
 
+    }
+
+
+    @Test
+    fun robotInGridTest() {
+
+        val G = arrayOf(
+            booleanArrayOf(true, true, false, true),
+            booleanArrayOf(false, true, true, false),
+            booleanArrayOf(true, false, true, true))
+
+        val robotInGrid = RobotIntoGrid()
+        val points = robotInGrid.robotInGrid(G)
+
+        Assert.assertEquals(6, points.size)
+        Assert.assertEquals(0, points[0].x)
+        Assert.assertEquals(0, points[0].y)
+
+        Assert.assertEquals(3, points[5].x)
+        Assert.assertEquals(2, points[5].y)
+
+
+        val pointsMemoization = robotInGrid.robotIntoGridMemoization(G)
+
+        Assert.assertEquals(6, pointsMemoization.size)
+        Assert.assertEquals(0, pointsMemoization[0].x)
+        Assert.assertEquals(0, pointsMemoization[0].y)
+
+        Assert.assertEquals(3, pointsMemoization[5].x)
+        Assert.assertEquals(2, pointsMemoization[5].y)
     }
 }
