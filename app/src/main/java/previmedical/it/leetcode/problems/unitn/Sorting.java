40b7533195994dc2dd1f5f56e72e9c872d1bfc1d
rigatol <rigatol@previnet.previdom.it>
2019-06-24 09:29:22 +0200

    Fix sorting algorithms

--- app/src/main/java/previmedical/it/leetcode/problems/unitn/Sorting.java
+++ app/src/main/java/previmedical/it/leetcode/problems/unitn/Sorting.java
@@ -11,8 +11,7 @@ import java.util.Random;
 public class Sorting {
 
 
-    // Class functions *************************************************************************************************
-
+    // BUBBLE SORT *****************************************************************************************************
     // Swaps subsequent elements if the right most element is greater than left one. n times
     // At the end of the first loop the maximum is at the end of the array.
     // Best case O(n): Already sorted array
@@ -38,12 +37,15 @@ public class Sorting {
     }
 
 
+    // MAX HEAP SORT ***************************************************************************************************
     // Since heapSort jumps from bottom to top of the heap many times,
     // it invalid any kind of caching on pages of memory.
     // That's why it works only for embedded system or when you don't have
     // such many items to sort.
     // It's complexity is O(n * log(n))
-    public int[] maxHeapSort(int[] heap) {
+    public int[] heapSort(int[] array) {
+        int[] heap = HeapUtil.buildMaxHeap(array);
+
         for (int i=0; i<heap.length; i++) {
             int length = heap.length - (i + 1);
             ArrayUtil.swap(heap, 0, length);
@@ -53,17 +55,18 @@ public class Sorting {
         return heap;
     }
 
-    public int[] minHeapSort(int[] heap) {
-        for (int i=0; i<heap.length; i++) {
-            int length = heap.length - (i + 1);
-            ArrayUtil.swap(heap, 0, length);
-            HeapUtil.minHeapRestore(heap, 0, length);
-        }
+//    public int[] minHeapSort(int[] heap) {
+//        for (int i=0; i<heap.length; i++) {
+//            int length = heap.length - (i + 1);
+//            ArrayUtil.swap(heap, 0, length);
+//            HeapUtil.minHeapRestore(heap, 0, length);
+//        }
+//
+//        return ArrayUtil.reverse(heap);
+//    }
 
-        return ArrayUtil.reverse(heap);
-    }
 
-
+    // INSERTION SORT **************************************************************************************************
     // Best case O(n): Already sorted array
     // Best case O(n2): Reverse sorted array
     // Very fast for small arrays (roughly 10)
@@ -83,10 +86,15 @@ public class Sorting {
     }
 
 
+    // MERGE SORT ******************************************************************************************************
     // Every case: O(n log n)
     // It has the problem that uses another array in order to merge.
     // Therefore it requires a space of O(n)
-    public int[] mergeSort(int[] array, int startIndex, int endIndex) {
+    public int[] mergeSort(int[] array) {
+        return this.mergeSort(array, 0, array.length - 1);
+    }
+
+    private int[] mergeSort(int[] array, int startIndex, int endIndex) {
 
         if (startIndex >= endIndex) {
             return new int[0];
@@ -101,7 +109,46 @@ public class Sorting {
         return array;
     }
 
+    private void merge(int[] array, int startIndex, int endIndex, int midIndex) {
+        int[] mergedArray = new int[endIndex - startIndex];
 
+        int indexArray1 = startIndex;
+        int indexArray2 = midIndex + 1;
+        int indexMergedArray = 0;
+
+        while (indexArray1 <= midIndex && indexArray2 <= endIndex) {
+
+            int value1 = array[indexArray1];
+            int value2 = array[indexArray2];
+
+            if (value1 < value2) {
+                mergedArray[indexMergedArray] = value1;
+                indexArray1++;
+            }
+            else {
+                mergedArray[indexMergedArray] = value2;
+                indexArray2++;
+            }
+
+            indexMergedArray++;
+        }
+
+        // Se non ho terminato gli elementi del sotto vettore di sinistra
+        // Allora li copio alla fine dell'array
+        int j = endIndex;
+        for (int i=midIndex; i>= indexArray1; i--) {
+            array[j] = array[i];
+            j--;
+        }
+
+        // Copio tutti gli elementi ordinati di merged array dall'inizio
+        for (int i=0; i<indexMergedArray; i++) {
+            array[i + startIndex] = mergedArray[i];
+        }
+    }
+
+
+    // QUICK SORT ******************************************************************************************************
     // Choose a pivot randomly. Than put all the lower elements to the right, and all the greater elements to the left.
     // Repeat recursively within the two halves.
     // Best case O(n * log2n): Prendo sempre il medio
@@ -147,6 +194,7 @@ public class Sorting {
     }
 
 
+    // SELECTION SORT **************************************************************************************************
     // Calculate the minimum value n times
     // Every case: O(n2)
     public int[] selectionSort(int[] array) {
@@ -160,47 +208,7 @@ public class Sorting {
     }
 
 
-
-
     // Private class functions *****************************************************************************************
-    private void merge(int[] array, int startIndex, int endIndex, int midIndex) {
-        int[] mergedArray = new int[endIndex - startIndex];
-
-        int indexArray1 = startIndex;
-        int indexArray2 = midIndex + 1;
-        int indexMergedArray = 0;
-
-        while (indexArray1 <= midIndex && indexArray2 <= endIndex) {
-
-            int value1 = array[indexArray1];
-            int value2 = array[indexArray2];
-
-            if (value1 < value2) {
-                mergedArray[indexMergedArray] = value1;
-                indexArray1++;
-            }
-            else {
-                mergedArray[indexMergedArray] = value2;
-                indexArray2++;
-            }
-
-            indexMergedArray++;
-        }
-
-        // Se non ho terminato gli elementi del sotto vettore di sinistra
-        // Allora li copio alla fine dell'array
-        int j = endIndex;
-        for (int i=midIndex; i>= indexArray1; i--) {
-            array[j] = array[i];
-            j--;
-        }
-
-        // Copio tutti gli elementi ordinati di merged array dall'inizio
-        for (int i=0; i<indexMergedArray; i++) {
-            array[i + startIndex] = mergedArray[i];
-        }
-    }
-
     private int minIndex(int[] array, int startIndex) {
         if (array.length == 0) {
             return -1;
--- app/src/test/java/previmedical/it/leetcode/unitn/SortingTest.kt
+++ app/src/test/java/previmedical/it/leetcode/unitn/SortingTest.kt
@@ -9,12 +9,67 @@ class SortingTest {
 
 
     @Test
+    fun bubbleSortTest() {
+
+        val sorting = Sorting()
+
+        val input = intArrayOf(12, 4, 9, 18, 2, 0, -5, 29, 1)
+        val output = sorting.bubbleSort(input)
+        val expected = intArrayOf(-5, 0, 1, 2, 4, 9, 12, 18, 29)
+        Assert.assertEquals(true, output!!.contentEquals(expected))
+    }
+
+    @Test
+    fun heapSortTest() {
+
+        val sorting = Sorting()
+
+        val input = intArrayOf(12, 4, 9, 18, 2, 0, -5, 29, 1)
+        val output = sorting.heapSort(input)
+        val expected = intArrayOf(-5, 0, 1, 2, 4, 9, 12, 18, 29)
+        Assert.assertEquals(true, output!!.contentEquals(expected))
+    }
+
+    @Test
+    fun insertionSortTest() {
+
+        val sorting = Sorting()
+
+        val input = intArrayOf(12, 4, 9, 18, 2, 0, -5, 29, 1)
+        val output = sorting.insertionSort(input)
+        val expected = intArrayOf(-5, 0, 1, 2, 4, 9, 12, 18, 29)
+        Assert.assertEquals(true, output!!.contentEquals(expected))
+    }
+
+    @Test
+    fun mergeSortTest() {
+
+        val sorting = Sorting()
+
+        val input = intArrayOf(12, 4, 9, 18, 2, 0, -5, 29, 1)
+        val output = sorting.mergeSort(input)
+        val expected = intArrayOf(-5, 0, 1, 2, 4, 9, 12, 18, 29)
+        Assert.assertEquals(true, output!!.contentEquals(expected))
+    }
+
+    @Test
     fun quickSortTest() {
 
         val sorting = Sorting()
 
         val input = intArrayOf(12, 4, 9, 18, 2, 0, -5, 29, 1)
         val output = sorting.quickSort(input)
+        val expected = intArrayOf(-5, 0, 1, 2, 4, 9, 12, 18, 29)
+        Assert.assertEquals(true, output!!.contentEquals(expected))
+    }
+
+    @Test
+    fun selectionSortTest() {
+
+        val sorting = Sorting()
+
+        val input = intArrayOf(12, 4, 9, 18, 2, 0, -5, 29, 1)
+        val output = sorting.selectionSort(input)
         val expected = intArrayOf(-5, 0, 1, 2, 4, 9, 12, 18, 29)
         Assert.assertEquals(true, output!!.contentEquals(expected))
     }
