117790aa57bb89e73665272eeafd546cede1980b
rigatol <rigatol@previnet.previdom.it>
2019-07-17 17:29:40 +0200

    -Add some moderate problems and start implement intersection problem

---
+++ app/src/main/java/previmedical/it/leetcode/problems/crack/moderate/Intersection.java
@@ -0,0 +1,46 @@
+package previmedical.it.leetcode.problems.crack.moderate;
+
+import previmedical.it.leetcode.models.Point;
+
+public class Intersection {
+
+    /*
+        Given two straight line segments (represented as a start point and an end point),
+        compute the point of intersection, if any.
+     */
+
+    /*
+
+        Solution 1.
+
+        1. Calculate the equation for both lines.
+        2. Get the intersection point, if exists
+        3. Verify if the intersection point is within the bounds
+            - Per entrambe le linee che x, y siano tra lo start ed end.
+
+        (0,0)  (2,4)
+
+     */
+
+
+    public Point intersection(Point l1a, Point l1b, Point l2a, Point l2b) {
+        int gradient1 = this.gradient(l1a, l1b);
+        int c1 = l1a.y - gradient1 * l1a.x;
+
+        int gradient2 = this.gradient(l2a, l2b);
+        int c2 = l2a.y - gradient2 * l2a.x;
+
+        // If they have different gradient they cannot intersect
+        if (gradient1 == gradient2 && c1 != c2) {
+            return null;
+        }
+
+        return l1a;
+    }
+
+
+    public int gradient(Point p1, Point p2) {
+        return (p2.x - p1.x) / (p2.y - p1.y);
+    }
+
+}
---
+++ app/src/test/java/previmedical/it/leetcode/crack/ModerateTests.kt
@@ -0,0 +1,49 @@
+package previmedical.it.leetcode.crack
+
+import android.util.Log
+import org.junit.Assert
+import org.junit.Test
+import previmedical.it.leetcode.models.Point
+import previmedical.it.leetcode.problems.crack.moderate.Intersection
+import previmedical.it.leetcode.problems.crack.moderate.SwapNumbers
+
+class ModerateTests {
+
+    @Test
+    fun swapNumbers() {
+
+        val swapNumbers = SwapNumbers()
+
+        val swap1 = swapNumbers.swapNumbers(SwapNumbers.Swap(1, 2))
+        Assert.assertEquals(2, swap1.a)
+        Assert.assertEquals(1, swap1.b)
+
+        val swap2 = swapNumbers.swapNumbers(SwapNumbers.Swap(5, 2))
+        Assert.assertEquals(2, swap2.a)
+        Assert.assertEquals(5, swap2.b)
+
+        val swap3 = swapNumbers.swapNumbers(SwapNumbers.Swap(-2, -3))
+        Assert.assertEquals(-3, swap3.a)
+        Assert.assertEquals(-2, swap3.b)
+    }
+
+
+    @Test
+    fun intersection() {
+
+        val p1a = Point(2,4)
+        val p1b = Point(4,8)
+
+        val p2a = Point(-1,0)
+        val p2b = Point(3,4)
+
+        val intersection = Intersection()
+
+        Assert.assertEquals(2, intersection.gradient(p1a, p1b))
+        Assert.assertEquals(1, intersection.gradient(p2a, p2b))
+
+        val output = intersection.intersection(p1a, p1b, p2a, p2b)
+
+        Log.w("ouput", "test")
+    }
+}
