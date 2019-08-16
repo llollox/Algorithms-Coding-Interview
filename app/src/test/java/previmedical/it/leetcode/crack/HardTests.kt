3a9a5c01b837073eda589da724755c07a13e34fc
llollox <lore91tanz@gmail.com>
2019-08-16 11:25:47 +0200

    Add letters and numbers problem

--- app/src/test/java/previmedical/it/leetcode/crack/HardTests.kt
+++ app/src/test/java/previmedical/it/leetcode/crack/HardTests.kt
@@ -3,12 +3,12 @@ package previmedical.it.leetcode.crack
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import previmedical.it.leetcode.problems.crack.hard.AddWithoutPlus
+import previmedical.it.leetcode.problems.crack.hard.LettersAndNumbers
 import previmedical.it.leetcode.problems.crack.hard.RandomSet
 import previmedical.it.leetcode.problems.crack.hard.ShuffleDeck
 
 class HardTests {
-
-
+    
     @Test
     fun addWithoutPlus() {
         val addWithoutPlus = AddWithoutPlus()
@@ -32,5 +32,13 @@ class HardTests {
         val randomSet = RandomSet()
         val output = randomSet.randomSet(intArrayOf(1,2,3,4,5,6,7,8,9,10), 5)
         assertEquals(5, output.size)
+    }
+
+    @Test
+    fun lettersAndNumbersTest() {
+        val lettersAndNumbers = LettersAndNumbers()
+        val array = charArrayOf('r', '1', 'a', 'c', '2', '3', 'q', 'q', 'r')
+        val output = lettersAndNumbers.lettersAndNumbers(array)
+        assertEquals(6, output.size)
     }
 }
