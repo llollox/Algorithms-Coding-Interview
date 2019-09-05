aba41b5ba35b37f481e77c3ad7aa93de9de6eb1a
llollox <lore91tanz@gmail.com>
2019-09-05 19:52:58 +0200

    Add is permutation with hash map

--- app/src/main/java/com/llollox/algorithms/problems/crack/strings/IsPermutation.java
+++ app/src/main/java/com/llollox/algorithms/problems/crack/strings/IsPermutation.java
@@ -1,6 +1,7 @@
 package com.llollox.algorithms.problems.crack.strings;
 
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class IsPermutation {
 
@@ -8,6 +9,7 @@ public class IsPermutation {
         Given two strings, write a method to decide if one is a permutation of the other.
      */
 
+    // Time O(n log n) Space O(1)
     public boolean isPermutation(String s1, String s2) {
         if (s1.length() != s2.length()) {
             return false;
@@ -20,6 +22,37 @@ public class IsPermutation {
         Arrays.sort(s2Array);
 
         return new String(s1Array).equals(new String(s2Array));
+    }
+
+    // Time O(n) Space O(n)
+    public boolean isPermutationHashMap(String s1, String s2) {
+        if (s1.length() != s2.length()) {
+            return false;
+        }
+
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (char c : s1.toCharArray()) {
+            Integer occurrences = map.get(c);
+            if (occurrences == null) {
+                occurrences = 0;
+            }
+            map.put(c, occurrences + 1);
+        }
+
+        for (char c : s2.toCharArray()) {
+            Integer occurrences = map.get(c);
+            if (occurrences == null) {
+                return false;
+            }
+            else if (occurrences == 1) {
+                map.remove(c);
+            }
+            else {
+                map.put(c, occurrences - 1);
+            }
+        }
+
+        return map.isEmpty();
     }
 
 }
